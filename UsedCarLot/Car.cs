4a682f57ceee945c30a8a9141a0ab1dc8fca330c
Elber Funez <elberfunez@gmail.com>
2022-08-01 21:33:39 -0400

    adding details

--- UsedCarLot/Car.cs
+++ UsedCarLot/Car.cs
@@ -3,9 +3,32 @@ namespace UsedCarLot
 {
 	public class Car
 	{
-		public Car()
+		public Car() // constructor w default values
 		{
+			Make = String.Empty;
+			Model = String.Empty;
+			Year = 0;
+			Price = 0;
 		}
-	}
+		public Car(string make, string model, int year, decimal price) // constructor that we define when we instantiate
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+			Price = price;
+
+		}
+		//properties
+		public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+		//public override string ToString() // overriding the toString method to get the format we want
+  //      {
+		//	return 
+  //      }
+		 
+    }
 }
 
