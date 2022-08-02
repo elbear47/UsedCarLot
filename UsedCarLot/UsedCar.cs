1c596abcb28ab5b95d81bf991eda8a5d8b65448d
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-08-01 21:51:04 -0400

    Added constructor

--- UsedCarLot/UsedCar.cs
+++ UsedCarLot/UsedCar.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace UsedCarLot
 {
-    internal class UsedCar:Car
+    public class UsedCar:Car
     {
         public double Mileage { get; set; }
 
-    }
+		public UsedCar(string make, string model, int year, decimal price,double mileage) // constructor that we define when we instantiate
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+			Price = price;
+			Mileage = mileage;
+		}
+	}
 }
